5f8e30e784e7641a1c3c48a02ab5a7c585691820
Timothy Chin <chinti@bc.edu>
2022-03-14 14:24:29 -0400

    Section 6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Timothy Chin on 3/14/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+}
